9ef13a3266853431bbe5b228a42e44252c815315
Manuel Andre Muñoz <andresmunoztodo@gmail.com>
2024-01-21 19:33:54 +0100

    Opcion 3, completa Opcion 4, por terminar Documentado

--- Java/EjercicioTienda/Articulo.java
+++ Java/EjercicioTienda/Articulo.java
@@ -5,23 +5,21 @@ public class Articulo {
     private String nombre;
     private double precioVentaCliente;
     private double precioCompraProveedor;
-    public final static double IVA = 1.21;
+    public final static double IVA = 0.21;
     private int stock;
     private static int contadorArticulos = 0;
+    private String nombreProveedor;
 
+     /**
+      * Contructor de inventario
+      * @param nombre
+      * @param precioVentaCliente
+      * @param precioCompraProveedor
+      * @param stock
+      * @param nombreProveedor
+      */
     
-
-    /**
-     * 
-     * @param identificador Identificador único
-     * @param nombre Articulo a introducir
-     * @param precioVentaCliente Precio de venta al cliente
-     * @param precioCompraProveedor Precio de compra al proveedor
-     * @param stock Stock mayor a 0
-     */
-    //Creo el constructor con una validacion de que los datos recibidos en el constructor son correctos; precio de venta > 0 y precio de venta, stock >= 0
-    
-    public Articulo(String nombre, double precioVentaCliente, double precioCompraProveedor, int stock) {
+    public Articulo(String nombre, double precioVentaCliente, double precioCompraProveedor, int stock, String nombreProveedor) {
         this.identificador = ++contadorArticulos;
         this.nombre = nombre;
         if (precioCompraProveedor > precioVentaCliente) {
@@ -29,7 +27,6 @@ public class Articulo {
         }
         this.precioVentaCliente = precioVentaCliente;
         this.precioCompraProveedor = precioCompraProveedor;
-        this.stock = stock;
         if (stock < 0) {
             System.err.println("El stock debe ser mayor o igual a cero");
         }
@@ -44,7 +41,7 @@ public class Articulo {
         return nombre;
     }
     public double getPrecioVentaCliente() {
-        return precioVentaCliente*IVA;
+        return (precioVentaCliente*IVA)+precioVentaCliente;
     }
     public void setPrecioVentaCliente(double precioVentaCliente) {
         this.precioVentaCliente = precioVentaCliente;
@@ -61,9 +58,19 @@ public class Articulo {
     public void setStock(int stock) {
         this.stock = stock;
     }
+    public String getNombreProveedor() {
+        return nombreProveedor;
+    }
+    public void setNombreProveedor(String nombreProveedor) {
+        this.nombreProveedor = nombreProveedor;
+    }
 
 
-    //Método vender articulo recibe como parametro la cantidad debe validar si fue exitosa o no la venta y reducir el stock
+    /**
+     * Método Verifica si hay stock suficiente y el cliente quiere comprar almenos una unidad. Si cumple, actualiza el stock
+     * @param cantidadAVender Cantidad que desea vender al cliente
+     * @return boolean 
+     */
     public boolean vender(int cantidadAVender){
         if (cantidadAVender>0 && stock>=cantidadAVender) {
             stock-=cantidadAVender;
@@ -74,7 +81,11 @@ public class Articulo {
         }
     }
 
-    //Método comprar articulo recibe como parametro la cantidad debe validar si fue exitosa o no la compra y aumentar el stock
+    /**
+     * Método Verifica si hay stock suficiente y la Tienda quiere comprar almenos una unidad. Si cumple, actualiza el stock
+     * @param cantidadAComprar Cantidad que desea comprar al proveedor
+     * @return
+     */
     public boolean comprar(int cantidadAComprar){
         if (cantidadAComprar>0) {
             stock+=cantidadAComprar;
@@ -85,7 +96,6 @@ public class Articulo {
         }
     }
     
-    //Método publico que devuelva un String con la informacion del articulo, probablemente con un toString DE UNA SOLA LINEA
     @Override
     public String toString() {
         return "Articulo [id=" + getIdentificador() + ", nombre=" + getNombre() + ", precioVentaCliente=" + getPrecioVentaCliente() + ", precioCompraProveedor="
--- Java/EjercicioTienda/Tienda.java
+++ Java/EjercicioTienda/Tienda.java
@@ -8,27 +8,23 @@ public class Tienda {
     static ArrayList<Articulo> articulosVendidos = new ArrayList<>();
 
     public static void main(String[] args) {
-        //Muestra la funcion menu menu()
-        //Pide la opcion un char pideOpcionMenuPrincipal()
-        //Mostrar el listado de Articulo de la tienda mostrarDescripcionArticulos()
-        //Hacer venta a cliente ventaACliente()
-
 
         //Creando instancias de Articulo
-        Articulo articuloUno = new Articulo("Zapatos",50, 5, 30);
-        Articulo articuloDos = new Articulo("Jean",40, 5, 20);
-        Articulo articuloTres = new Articulo("Camiseta", 27, 7, 4);
+        Articulo articuloUno = new Articulo("Zapatos",50, 5, 30, "Nike");
+        Articulo articuloDos = new Articulo("Jean",40, 5, 20, "Jhon");
+        Articulo articuloTres = new Articulo("Camiseta", 27, 7, 4, "Carlos");
 
         //Añadiendo articulos al ArrayList de tipo Articulo
         inventario.add(articuloUno);
         inventario.add(articuloDos);
         inventario.add(articuloTres);
 
-
         menu();
     }
-    
 
+    /**
+     * Menú principal Tienda
+     */
     public static void menu(){
             do {
                 System.out.println("""
@@ -49,19 +45,28 @@ public class Tienda {
                         System.out.println("---------------------------------------------------------------------------");
                         System.out.println("-----------------------  HACIENDO VENTA A CLIENTE  ------------------------");
                         System.out.println("---------------------------------------------------------------------------");
-                        venderACliente(3, 1, "Manuel");
+                        venderACliente("Manuel");
                     break;
                     case 3:
                         System.out.println("---------------------------------------------------------------------------");
                         System.out.println("-----------------------  HACIENDO COMPRA A PROVEEDOR ----------------------");
                         System.out.println("---------------------------------------------------------------------------");
+                        comprarAProveedor();
+                    break;
+                    case 4:
+                        anadirArticulo();
                     break;
                     default:
+                        System.err.println("Valor inválido");
                     break;
                 }      
             } while (true);
     }
 
+    /**
+     * Pide, valida y devuelte el número si es entero entre 1-5
+     * @return int
+     */
     public static int pideOpcionMenuPrincipal() {
         Scanner sc = new Scanner(System.in);
         int opcion = 0;
@@ -80,48 +85,192 @@ public class Tienda {
         return opcion;
     }
 
-    //La opción 1 mostrará por pantalla la descripción de todos los artículos de la
-    //tienda.
+    /**
+     * Muestra el listado de articulos en la Tienda
+     * @param arrayListArticulos Inventario
+     */
     public static void mostrarDescripcionArticulos(ArrayList<Articulo> arrayListArticulos){
         for (Articulo articulo : arrayListArticulos) {
             System.out.println(articulo);
         }
     }
 
-    //La opción 2 permitirá realizar una venta. Pedirá los identificadores y cantidades
-    //de los artículos deseados además del nombre del cliente. Mostrará el precio total y
-    //pedirá confirmar la venta.
+    /**
+     * Busca un elemento por su indice y lo devuelve de acuerdo al indice ingresado
+     * @param elementosFactura Objeto con la información necesaria para la creación de una factura
+     * @param indexABuscar Indice del Objeto que desea devolver
+     * @return Objeto que contiene un valor de tipo int, double o String
+     */
+    private static Object buscarResumen(ArrayList<Object> elementosFactura, int indexABuscar) {
+        return elementosFactura.get(indexABuscar);
+    }
 
-    public static boolean venderACliente(int identificador, int cantidadAComprar, String nombreCliente){
-        double precioTotal = 0;
-        boolean confirmarVenta = false;
-        double subtotal = 0;
-        int seRepite = 0;
+
+    /**
+     * Trae cualquier articulo en el inventario por su identificador
+     * @param identificador Recibe el identificador a buscar
+     * @return Articulo si existe
+     */
+    private static Articulo buscarArticuloPorIdentificador(int identificador) {
         for (Articulo articulo : inventario) {
-            if (identificador==articulo.getIdentificador()) {
-                articulosVendidos.add(articulo);
-                subtotal = subtotal + articulo.getPrecioVentaCliente();
-                confirmarVenta = articulo.vender(cantidadAComprar);
-                for (Articulo vendido : articulosVendidos) {
-                    if(articulo.equals(vendido)){
-                        seRepite++;
-                    }
-                }
+            if (articulo.getIdentificador() == identificador) {
+                return articulo;
             }
-            precioTotal=precioTotal+subtotal;
         }
-        if (confirmarVenta) {
-            System.out.println("La compra de " + nombreCliente + " fue: ");
-            for (Articulo articulo : articulosVendidos) {
-                System.out.println(articulo);
-            }
-            precioTotal = precioTotal * seRepite;
-            subtotal = precioTotal/1.21;
-            System.out.println("Precio subtotal: " + subtotal + " unidades: " + seRepite + " el precio Total es de: " + precioTotal);
+        return null;
+    }
+
+    /**
+     * Realiza la venta al cliente si la confirmaición es exitosa
+     * @param nombreCliente
+     */
+    public static void venderACliente(String nombreCliente){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el identificador del articulo: ");
+        int identificador = sc.nextInt();
+
+        System.out.println("Ingrese la cantidad a comprar: ");
+        int cantidadAComprar = sc.nextInt();
+
+        Articulo articuloBuscado = buscarArticuloPorIdentificador(identificador);
+        ArrayList<Object> elementosFactura = new ArrayList<>();
+        elementosFactura.add(articuloBuscado.getNombre());
+        elementosFactura.add(articuloBuscado.getPrecioVentaCliente());
+        elementosFactura.add(cantidadAComprar);
+        
+        if (confirmaVenta(elementosFactura)) {
+            articuloBuscado.vender(cantidadAComprar);
+        }
+    }
+
+    /**
+     * Confirma o cancela la compra
+     * @param elementosFactura Informacion del articulo buscado por el comprador
+     * @return boolean Si el comprador aprueba la compra
+     */
+    public static boolean confirmaVenta(ArrayList<Object> elementosFactura) {
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.println("Artículo: " + buscarResumen(elementosFactura, 0));
+        System.out.println("Precio Unitario: " + buscarResumen(elementosFactura, 1));
+        System.out.println("Cantidad: " + buscarResumen(elementosFactura, 2));
+    
+        double precioUnitario = ((Number) buscarResumen(elementosFactura, 1)).doubleValue();
+        double cantidadComprada = ((Number) buscarResumen(elementosFactura, 2)).doubleValue();
+
+        double precioTotal = precioUnitario * cantidadComprada;
+        System.out.println("Precio total: " + precioTotal);
+    
+        System.out.println("¿Confirmar la compra? (S/N)");
+        String respuesta = sc.next().toUpperCase();
+    
+        if (respuesta.equals("S")) {
+            System.out.println("Compra realizada con éxito");
+            return true;
         } else {
-            System.out.println("No se pudo completar la venta.");
+            System.out.println("Compra cancelada");
+            return false;
         }
-        return confirmarVenta;
+    }
+
+    /**
+     * Pide la informacion necesaria para hacer una compra y realiza la compra actualizando los inventarios y pidiendo confirmacion al comprador
+     */
+    public static void comprarAProveedor() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese identificador del artículo:");
+        int identificador = sc.nextInt();
+        Articulo articulo = buscarArticuloPorIdentificador(identificador);
+        if (articulo != null) {
+
+            System.out.println("Ingrese cantidad a comprar:");
+            int cantidadAComprar = sc.nextInt();
+
+            System.out.println("Ingrese nombre del proveedor:");
+            String nombreProveedor = sc.next();
+
+            double precioTotal = articulo.getPrecioCompraProveedor() * cantidadAComprar;
+            System.out.println("Precio total: " + precioTotal);
+            System.out.println("¿Confirmar la compra? (S/N)");
+
+            String respuesta = sc.next().toUpperCase();
+            articulo.setNombreProveedor(nombreProveedor);
+            if (respuesta.equals("S")) {
+                articulo.comprar(cantidadAComprar);
+                System.out.println("Compra realizada con éxito.");
+            } else {
+                System.out.println("Compra cancelada.");
+            }
+        } else {
+            System.out.println("Artículo no encontrado.");
+        }
+    }
+
+    /**
+     * Añade articulo por identificador
+     */
+    public static void anadirArticulo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese nombre del artículo:");
+        String nombre = sc.nextLine();
+        System.out.println("Ingrese precio de venta al cliente:");
+        double precioVentaCliente = sc.nextDouble();
+        System.out.println("Ingrese precio de compra al proveedor:");
+        double precioCompraProveedor = sc.nextDouble();
+        System.out.println("Ingrese stock:");
+        int stock = sc.nextInt();
+        System.out.println("Ingrese el proveedor:");
+        String nombreProveedor = sc.nextLine();
+
+        Articulo nuevoArticulo = new Articulo(nombre, precioVentaCliente, precioCompraProveedor, stock, nombreProveedor);
+        inventario.add(nuevoArticulo);
+
+        System.out.println("Artículo añadido con éxito.");
+    }
+
+    /**
+     * Edita articulo por identificador
+     */
+    public static void editarArticulo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese identificador del artículo a editar:");
+        int identificador = sc.nextInt();
+
+        Articulo articulo = buscarArticuloPorIdentificador(identificador);
+        if (articulo != null) {
+            
+            System.out.println("Ingrese nuevo precio de venta al cliente:");
+            double nuevoPrecioVentaCliente = sc.nextDouble();
+            System.out.println("Ingrese nuevo precio de compra al proveedor:");
+            double nuevoPrecioCompraProveedor = sc.nextDouble();
+            System.out.println("Ingrese nuevo stock:");
+            int nuevoStock = sc.nextInt();
+
+            articulo.setPrecioVentaCliente(nuevoPrecioVentaCliente);
+            articulo.setPrecioCompraProveedor(nuevoPrecioCompraProveedor);
+            articulo.setStock(nuevoStock);
+            System.out.println("Artículo editado con éxito.");
+        } else {
+            System.out.println("Artículo no encontrado.");
+        }
+    }
+
+    /**
+     * Elimina articulo por identificador
+     */
+    public static void eliminarArticulo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese identificador del artículo a eliminar:");
+        int identificador = sc.nextInt();
+
+        Articulo articulo = buscarArticuloPorIdentificador(identificador);
+        if (articulo != null) {
+            inventario.remove(articulo);
+            System.out.println("Artículo eliminado con éxito.");
+        } else {
+            System.out.println("Artículo no encontrado.");
+        }
     }
 
 }
