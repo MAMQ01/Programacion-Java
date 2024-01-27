46e8bf94358bf75898ff1d71c4c245cdb89015e8
Manuel Andre Muñoz <andresmunoztodo@gmail.com>
2024-01-27 22:24:12 +0100

    Empezando el nueve

---
+++ Java/EJerciciosArrayList/Distribucion.java
@@ -0,0 +1,5 @@
+package EJerciciosArrayList;
+
+public class Distribucion {
+    
+}
---
+++ Java/EJerciciosArrayList/EjercicioDiez.java
@@ -0,0 +1,26 @@
+package EJerciciosArrayList;
+
+public class EjercicioDiez {
+    
+/* Ejercicio 10
+Implementa un programa que cree una matriz de tamaño F x C (estos valores se
+introducirán como parámetros en la línea de comandos) y la rellene con números
+enteros aleatorios entre 1 y 20. Luego pedirá por teclado un número V y mostrará por
+pantalla cuántos valores de cada fila son múltiplos de V. Supondremos que el usuario
+introducirá valores válidos por lo que no será necesario comprobarlos.
+Por ejemplo, dada esta matriz de 3x3 y V=5
+5 10 9
+1 6 19
+20 15 10
+El programa mostrará por pantalla:
+Fila 1: 2
+Fila 2: 0
+Fila 3: 3 */
+
+/* Ejercicio 11
+Realiza un programa que construya una matriz de 3x3 y la rellene aleatoriamente de
+números 0 o 1. El programa mostrará el tablero que se ha generado y cuantas ‘3 en raya’
+tienen los 0’s y cuantas tienen los 1’s, indicando quien en el ganador y si han empatado.
+Hay que tener en cuenta las filas, las columnas y las diagonales principal y secundaria.
+Por ejemplo, dada esta matriz, mostrará Lineas 0: 2 Lineas 1: 0 Gana 0 */
+}
